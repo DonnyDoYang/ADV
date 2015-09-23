40ed3156fa5a4d129e26cf9206c7911b4b6182e8
wmmeng <wmmeng8@gmail.com>
2015-09-23 10:52:55 +0800

    call super.onCreate() of App

--- app/src/main/java/kdui/uj/tkd/kdg/App.java
+++ app/src/main/java/kdui/uj/tkd/kdg/App.java
@@ -3,7 +3,7 @@ package kdui.uj.tkd.kdg;
 import android.app.Application;
 public class App extends Application {
 	public void onCreate() {
-		
+		super.onCreate();
 		new AbstractApp().onCreate(getApplicationContext());
-	};
+	}
 }
